86014b978f3dc0bd83ffe45cbcdd8024b7290b36
jannes-m <malnamalja@gmx.de>
2017-06-20 16:56:09 +0200

    cv as pdf

--- write_cv.R
+++ write_cv.R
@@ -5,5 +5,5 @@ content <- system.file("content/muenchow_full_vita.yaml", package = "vita")
 # content <-  system.file("content/muenchow_vita_en.yaml", package = "vita")
 # content <- "D:/programming/R/cv/inst/content/muenchow_vita_en.yaml"
 style <- system.file("style/dl-vita.sty", package = "vita")
-out <- "C:/Users/pi37pat/Desktop/outdir2"
+out <- "D:/programming/R/cv/out/"
 build_cv(content, style, out, clean = TRUE)
