c978796386effce4936aa5b1e6dcaf57178917d8
jannes-m <malnamalja@gmx.de>
2018-03-13 14:58:10 +0100

    update cv

--- write_cv.R
+++ write_cv.R
@@ -5,6 +5,6 @@ content <- system.file("content/muenchow_full_vita.yaml", package = "vita")
 # content <-  system.file("content/muenchow_vita_en.yaml", package = "vita")
 # content <- "D:/programming/R/cv/inst/content/muenchow_vita_en.yaml"
 style <- system.file("style/dl-vita.sty", package = "vita")
-out <- "D:/programming/R/cv/out/"
+out <- "D:/programming/R/cv/pdf/"
 build_cv(content, style, out, clean = TRUE)
 
