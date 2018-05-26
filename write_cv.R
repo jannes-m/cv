8e924613c119128922db9caee6184d60088fa0da
jannes-m <malnamalja@gmx.de>
2018-05-26 17:01:03 +0200

    replace <- by =

--- write_cv.R
+++ write_cv.R
@@ -1,10 +1,10 @@
 # generate CV
 devtools::load_all()
-# content <- "D:/programming/R/cv/inst/content/muenchow_full_vita.yaml"
-content <- system.file("content/muenchow_full_vita.yaml", package = "vita")
-# content <-  system.file("content/muenchow_vita_en.yaml", package = "vita")
-# content <- "D:/programming/R/cv/inst/content/muenchow_vita_en.yaml"
-style <- system.file("style/dl-vita.sty", package = "vita")
-out <- "D:/programming/R/cv/pdf/"
+# content = "D:/programming/R/cv/inst/content/muenchow_full_vita.yaml"
+content = system.file("content/muenchow_vita_munich_re.yaml", package = "vita")
+# content =  system.file("content/muenchow_vita_en.yaml", package = "vita")
+# content = "D:/programming/R/cv/inst/content/muenchow_vita_en.yaml"
+style = system.file("style/dl-vita.sty", package = "vita")
+out = "D:/programming/R/cv/pdf/"
 build_cv(content, style, out, clean = TRUE)
 
