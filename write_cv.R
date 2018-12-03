79daa7e8d8f6d26be090bc3f2d00fcc451b99e8a
jannes-m <malnamalja@gmx.de>
2018-12-03 09:29:23 +0100

    update write_cv

--- write_cv.R
+++ write_cv.R
@@ -4,6 +4,8 @@ devtools::load_all()
 content = "inst/content/muenchow_full_vita.yaml"
 style = system.file("style/dl-vita.sty", package = "vita")
 out = "pdf/"
-build_cv(content, style, out, pub_score = TRUE, clean = TRUE)
-
-
+# If you want to scrape the WOS, you need to figure out the sid for your
+# query... haven't found a completetely automated way so far, hence, go to the
+# WOS, query au=muenchow,j and copy the sid from the resulting url (grrrr)
+build_cv(content, style, out, pub_score = TRUE, sid = "C5a9zLFedsAog9BGOBX",
+         clean = TRUE)
