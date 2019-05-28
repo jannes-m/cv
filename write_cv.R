758afde31ee2bfa71785b2c463d724f374063721
jannes-m <malnamalja@gmx.de>
2019-05-28 10:01:37 +0200

    add wos explanation

--- write_cv.R
+++ write_cv.R
@@ -1,11 +1,28 @@
 # generate CV
 devtools::load_all()
 
-content = "inst/content/muenchow_full_vita.yaml"
+content = "inst/content/dfg/muenchow_dfg.yaml"
+content = "inst/content/muenchow_full/muenchow_full_vita.yaml"
 style = system.file("style/dl-vita.sty", package = "vita")
 out = "pdf/"
 # If you want to scrape the WOS, you need to figure out the sid for your
 # query... haven't found a completetely automated way so far, hence, go to the
 # WOS, query au=muenchow,j and copy the sid from the resulting url (grrrr)
-build_cv(content, style, out, pub_score = TRUE, sid = "C5a9zLFedsAog9BGOBX",
-         clean = TRUE)
+
+# it seems, that get_pub_record() no longer works for WOS...
+# well, I didn't like the solution in any case. If you really need it again,
+# just copy and paste from WOS...
+# build_cv(content, style, out, pub_score = TRUE, sid = "F4vpP3iHDTxP21bPh3j",
+#          clean = FALSE)
+
+build_cv(content, style, out, pub_score = FALSE, clean = TRUE)
+# If you want to scrape the WOS, you need to figure out the sid for your
+# query... haven't found a completetely automated way so far, hence, go to the
+# WOS, query au=muenchow,j and copy the sid from the resulting url (grrrr)
+
+# it seems, that get_pub_record() no longer works for WOS...
+# well, I didn't like the solution in any case. If you really need it again,
+# just copy and paste from WOS...
+# build_cv(content, style, out, pub_score = TRUE, sid = "F4vpP3iHDTxP21bPh3j",
+#          clean = FALSE)
+
